386dbc3d634972e22104b24648c89b1a3e8a358b
Merlijn Tishauser <merlijn@gargleblaster.org>
2018-02-16 10:01:07 +0100

    added project structure and setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='gener8',
+    version='0.1',
+    description='Generates a Dockerfile template based on user input',
+    packages=[
+        'gener8',
+        'gener8.templates'
+    ],
+    package_data={
+        'gener8.templates': ['*.jinja2']
+    },
+    scripts=['bin/gener8'],
+    install_requires=[
+        "certifi",
+        "click",
+        "click-log",
+        "chardet",
+        "Jinja2",
+        "idna",
+        "MarkupSafe",
+        "pip",
+        "requests",
+        "setuptools",
+        "urllib3",
+        "wheel"
+    ],
+    extras_require={
+        'test': [
+            "mock",
+            "pytest",
+            "pytest-mock",
+            "pytest-cov",
+            "tzlocal"
+        ]
+    },
+    zip_safe=False
+)
