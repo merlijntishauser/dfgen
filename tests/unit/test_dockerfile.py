f0e68655d74ab5893793b6440280f94535991afe
Merlijn Tishauser <merlijn@gargleblaster.org>
2018-02-27 16:11:35 +0100

    added some basic tests

--- tests/unit/test_dockerfile.py
+++ tests/unit/test_dockerfile.py
@@ -12,3 +12,16 @@ def teardown():
 
 def test_set_linux_type_returns_base_image():
     assert Dockerfile().set_linux_type('alpine') == 'alpine:3.6'
+
+
+def test_set_command_splits_string_at_spaces():
+    dockerfile = Dockerfile()
+    dockerfile.set_command("should --be --splitted")
+    assert dockerfile.template_variables["command"] == ['should', '--be', '--splitted']
+
+
+def test_setting_description_sets_default_labels():
+    dockerfile = Dockerfile()
+    dockerfile.set_description("a description")
+    dockerfile.render_template()
+    assert 'org.label-schema.schema-version="1.0.0-rc1"' == dockerfile.labels["schema-version"]
